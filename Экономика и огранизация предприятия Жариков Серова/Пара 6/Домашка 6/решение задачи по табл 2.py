f9f5f2c00927ef8905956c5b47b33b334b33c9af
Samarcev Anton <samarcev.anton1@gmail.com>
2017-04-17 20:31:16 +0300

    Added 8 lecture by to economy

---
+++ Экономика и огранизация предприятия Жариков Серова/Пара 6/Домашка 6/решение задачи по табл 2.py
@@ -0,0 +1,18 @@
+
+
+def sayHello():
+	print ("Hello")
+
+def slove():
+	obs = []
+	inp = None;
+	for i in range(12):
+		inp = input("Get obs")
+		obs.append( int( obs ) );
+
+	# посчитаем среднеквартальные остатки
+	for i 
+	
+if __name__ == "__main__":
+	sayHello()
+	input()
